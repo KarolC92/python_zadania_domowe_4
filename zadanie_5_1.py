b16c5cd1550805c68bf2dc534cb6b39ebc8bb5af
Karol Ćwiąkała <karol.cwiakala@gmail.com>
2021-06-23 17:40:23 +0200

    zadanie 5.1.1

--- zadanie_5_1.py
+++ zadanie_5_1.py
@@ -17,59 +17,29 @@ USA – 1
 4. jak wyżej, ale liczy jeszcze dla każdego kraju średni wzrost zawodników."""
 
 
-def height(open_file, operand: str):
+def file_spliting(open_file, operand):
     """
-    function to get the highest or smaller 'ski-jumper'
-    :param open_file: csv format file
-    :param operand: if you want to get highest enter: '>', to get the smaller, enter: '<'
-    :return: list with: name, surname, nationality, date_of_born, height and weight of highest or smaller ski jumper
-    """
-    result = []
-    for i in open_file.readlines():
-        if result:
-            if operand == '>':
-                if i.strip().split(';')[4] > result[4]:
-                    result = i.strip().split(';')
-            elif operand == '<':
-                if i.strip().split(';')[4] < result[4]:
-                    result = i.strip().split(';')
-        else:
-            result = i.strip().split(';')
-    return result
-
-
-def weight(open_file, operand: str):
-    """
-    function to get the heaviest or the lightest 'ski-jumper'
-    :param open_file: csv format file
-    :param operand: if you want to get the heaviest enter: '>', to get the lightest, enter: '<'
-    :return: list with: name, surname, nationality, date_of_born, height and weight of the heaviest or the lightest ski
-    jumper
-    """
-    result = []
-    for i in open_file.readlines():
-        if result:
-            if operand == '>':
-                if i.strip().split(';')[5] > result[5]:
-                    result = i.strip().split(';')
-            elif operand == '<':
-                if i.strip().split(';')[5] < result[5]:
-                    result = i.strip().split(';')
-        else:
-            result = i.strip().split(';')
-    return result
-
+     function to get the heaviest or the lightest 'ski-jumper'
+     :param open_file: csv format file
+     :param operand: if you want to get the highest one, enter: 'highest', to get the smallest, enter: 'smallest',
+     if you want to get the heaviest one, enter: 'heaviest', else enter: 'lightest'
+     :return: list with: name, surname, nationality, date_of_born, height and weight of the sorted ski-jumpers
+     """
+    res = sorted([l.strip().split(';') for l in open_file.readlines()],
+                 key=(lambda x: x[4]) if operand in ('highest', 'smallest') else (lambda x: x[5]),
+                 reverse=operand in ('highest', 'heaviest'))
+    return res[0]
 
 with open('baza_danych/zawodnicy.csv', 'r', encoding='UTF-8') as file:
-    most_h = height(file, '>')
+    most_h = file_spliting(file, 'highest')
     print(f'Highest ski jumper on the list is {most_h[0]} {most_h[1]}, his height = {most_h[4]}')
     file.seek(0)
-    smallest_ski_j = height(file, '<')
+    smallest_ski_j = file_spliting(file, 'smallest')
     print(
         f'Smallest ski jumper on the list is {smallest_ski_j[0]} {smallest_ski_j[1]}, his height = {smallest_ski_j[4]}')
     file.seek(0)
-    the_heaviest = weight(file, '>')
+    the_heaviest = file_spliting(file, 'heaviest')
     print(f'The heaviest ski jumper on the list is {the_heaviest[0]} {the_heaviest[1]}, his weight = {the_heaviest[5]}')
     file.seek(0)
-    the_lightest = weight(file, '<')
+    the_lightest = file_spliting(file, 'lightest')
     print(f'The lightest ski jumper on the list is {the_lightest[0]} {the_lightest[1]}, his weight = {the_lightest[5]}')
